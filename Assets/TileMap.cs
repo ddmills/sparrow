c5344763accba434a03f45c2fc8cd94f6e738c25
ddmills <mills.dalton@gmail.com>
2017-05-31 22:39:02 -0500

    generate simple tiles

---
+++ Assets/TileMap.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class TileMap : MonoBehaviour {
+
+  public int width;
+  public int height;
+  public int tileWidth;
+  public int tileHeight;
+
+
+  void Start () {
+    GenerateMesh();
+  }
+
+  void GenerateMesh() {
+    Mesh mesh = new Mesh();
+
+    Vector3[] vertices = new Vector3[(width + 1) * (height + 1)];
+    int[] triangles = new int[width * height * 6];
+
+    for (int i = 0, z = 0; z <= height; z++) {
+      for (int x = 0; x <= width; x++, i++) {
+        vertices[i] = new Vector3(x, 0, z);
+      }
+    }
+
+    for (int i = 0, v = 0, y = 0; y < height; y++, v++) {
+      for (int x = 0; x < width; x++, i += 6, v++) {
+        triangles[i] = v;
+        triangles[i + 3] = triangles[i + 2] = v + 1;
+        triangles[i + 4] = triangles[i + 1] = v + width + 1;
+        triangles[i + 5] = v + width + 2;
+      }
+    }
+
+    mesh.vertices = vertices;
+    mesh.triangles = triangles;
+
+    GetComponent<MeshFilter>().mesh = mesh;
+  }
+}
